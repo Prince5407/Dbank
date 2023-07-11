c63557aa38a09b906d733084cfce2a8daf0488cd
Prince5407 <103447465+Prince5407@users.noreply.github.com>
2023-07-11 15:58:59 +0530

    Delete src/declarations directory

--- src/declarations/Bank_backend/Bank_backend.did.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {
-  'checkBalance' : ActorMethod<[], number>,
-  'compound' : ActorMethod<[], undefined>,
-  'topUp' : ActorMethod<[number], undefined>,
-  'withdraw' : ActorMethod<[number], undefined>,
-}
